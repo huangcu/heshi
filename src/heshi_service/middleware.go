a6dc63b639adf7f1bcaf70ddc40087ae1cd500c7
Cunying Huang <cunying.huang@hpe.com>
2017-12-25 11:12:16 +0800

    cors

--- src/heshi_service/middleware.go
+++ src/heshi_service/middleware.go
@@ -3,24 +3,26 @@ package main
 import (
 	"net/http"
 	"os"
+	"time"
 	"util"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// c.Writer.Header().Set("Content-Type", "application/json")
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		// c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		// if c.Req.Method == "OPTIONS" {
-		// 	fmt.Println("options")
-		// 	c.Abort(200)
-		// 	return
-		// }
-		c.Next()
-	}
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"https://github.com"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		// AllowOriginFunc: func(origin string) bool {
+		// 	return origin == "https://github.com"
+		// },
+		MaxAge: 12 * time.Hour,
+	})
 }
 
 func AuthMiddleWare() gin.HandlerFunc {
