a59a1a35645bb188caaf4f5c94976abd29e8b3f9
Cunying Huang <cunying.huang@hpe.com>
2018-01-20 14:33:56 +0800

    discount created_by

--- src/heshi_service/discount.go
+++ src/heshi_service/discount.go
@@ -15,17 +15,20 @@ import (
 type discount struct {
 	DiscountCode string    `form:"discount_code" json:"discount_code" binding:"required"`
 	Discount     int       `form:"discount" json:"discount" binding:"required"`
+	CreatedBy    string    `json:"created_by"`
 	CreatedAt    time.Time `json:"created_at"`
 }
 
 func newDiscount(c *gin.Context) {
+	createdBy := c.MustGet("id").(string)
 	var nd discount
 	if err := c.ShouldBind(&nd); err != nil {
 		c.JSON(http.StatusOK, VEMSG_AGENT_DISCOUNT_NOT_VALID)
 		return
 	}
 	id := uuid.NewV4().String()
-	q := fmt.Sprintf(`INSERT INTO discounts (id, discount_code, discount) VALUES ('%s', '%s', '%d')`, id, nd.DiscountCode, nd.Discount)
+	q := fmt.Sprintf(`INSERT INTO discounts (id, discount_code, discount, created_by) 
+	VALUES ('%s', '%s', '%d', '%s')`, id, nd.DiscountCode, nd.Discount, createdBy)
 	if _, err := dbExec(q); err != nil {
 		c.JSON(http.StatusBadRequest, errors.GetMessage(err))
 		return
@@ -36,11 +39,11 @@ func newDiscount(c *gin.Context) {
 
 func getDiscount(c *gin.Context) {
 	id := c.Param("id")
-	var discountCode string
+	var discountCode, createdBy string
 	var discountNumber int
 	var createdAt time.Time
-	q := fmt.Sprintf(`SELECT discount_code, discount,created_at FROM discounts WHERE id = '%s'`, id)
-	if err := dbQueryRow(q).Scan(&discountCode, &discountNumber, &createdAt); err != nil {
+	q := fmt.Sprintf(`SELECT discount_code, discount, created_at, createdBy FROM discounts WHERE id = '%s'`, id)
+	if err := dbQueryRow(q).Scan(&discountCode, &discountNumber, &createdAt, &createdBy); err != nil {
 		if err == sql.ErrNoRows {
 			c.JSON(http.StatusOK, VEMSG_DISCOUNT_NOT_EXIST)
 			return
@@ -51,6 +54,7 @@ func getDiscount(c *gin.Context) {
 	d := discount{
 		DiscountCode: discountCode,
 		Discount:     discountNumber,
+		CreatedBy:    createdBy,
 		CreatedAt:    createdAt.Local(),
 	}
 	c.JSON(http.StatusOK, d)
@@ -65,16 +69,17 @@ func getAllDiscounts(c *gin.Context) {
 	}
 	var ds []discount
 	for rows.Next() {
-		var discountCode string
+		var discountCode, createdBy string
 		var discountNumber int
 		var createdAt time.Time
-		if err := rows.Scan(&discountCode, &discountNumber, &createdAt); err != nil {
+		if err := rows.Scan(&discountCode, &discountNumber, &createdAt, &createdBy); err != nil {
 			c.JSON(http.StatusInternalServerError, err.Error())
 			return
 		}
 		d := discount{
 			DiscountCode: discountCode,
 			Discount:     discountNumber,
+			CreatedBy:    createdBy,
 			CreatedAt:    createdAt.Local(),
 		}
 		ds = append(ds, d)
