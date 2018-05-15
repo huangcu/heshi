a27267056fe0e92ce28e2d0dc92c664dc4d237b8
Cunying Huang <cunying.huang@hpe.com>
2018-05-15 12:53:33 +0800

    env

--- env_windows.bash
+++ env_windows.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export PROJECT_DIR="$(gb env GB_PROJECT_DIR)"
-export GOPATH="$PROJECT_DIR\vendor;$PROJECT_DIR"
+export GOPATH="$PROJECT_DIR\vendor;$PROJECT_DIR;$GOPATH"
 export PATH="$PROJECT_DIR\bin:$PROJECT_DIR\deploy:$HOME\gopath\bin:$PATH"
